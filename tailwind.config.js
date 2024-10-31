b5f6a7792c1021ab370ba4d10fafa41d95aa13f4
Julios Leon <julios.leon@cirvianum.cat>
2024-10-31 16:01:05 +0100

    Revert "tailwind.config + login bg"
    This reverts commit 1fe6e12d73571d427287b6487869843b422291e2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./html/**/*.html", "./index.html"],
+  content: ["./public/**/*.html", "./index.html"],
   theme: {
     extend: {
       colors: {
