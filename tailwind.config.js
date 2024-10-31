d1a01665f7ccea3138807693c8ab556bcb606ce5
Pol Romeu <pol.romeu@cirvianum.cat>
2024-10-31 16:10:59 +0100

    yow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./index.html"],
+  content: ["./public/**/*.html", "./html/**/*.html", "./index.html"],
   theme: {
     extend: {
       colors: {
-        'custom-blue': '#4B86B4',
-        'textColor': '#EDF6F9',
-      }
+        "custom-blue": "#4B86B4",
+        textColor: "#EDF6F9",
+      },
     },
   },
   plugins: [],
