939d39bc1dae53c669454664ff81eed28a8171ce
Pol Romeu <pol.romeu@cirvianum.cat>
2024-11-05 20:05:43 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  root: ".", // Root directory for Vite
+  root: "./", // Directori root de Vite
+  base: "./", // Rutes relatives per als assets
+  build: {
+    outDir: "dist", // Directori de sortida per al build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"), // Pàgina principal
+        AboutUS: path.resolve(__dirname, "html/AboutUs.html"),
+        Gallery: path.resolve(__dirname, "html/Gallery.html"),
+        JoinUs: path.resolve(__dirname, "html/JoinUs.html"),
+        login: path.resolve(__dirname, "html/login.html"),
+        Merch: path.resolve(__dirname, "html/Merch.html"),
+        Product: path.resolve(__dirname, "html/Product.html"),
+        login: path.resolve(__dirname, "src/pages/login.html"),
+        News: path.resolve(__dirname, "html/News.html"),
+        register: path.resolve(__dirname, "html/register.html"),
+        WhatWeDo: path.resolve(__dirname, "html/WhatWeDo.html"),
+      },
+    },
+  },
   server: {
     watch: {
-      usePolling: true, // Ensures changes are detected in some environments
+      usePolling: true, // Per assegurar que els canvis es detecten en alguns entorns
     },
   },
 });
